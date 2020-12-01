0963ca1fb5a6b21b6bdedcf2112e2d321ac01a67
Marcin Parda <marcin98parda@gmail.com>
2020-12-02 00:16:42 +0100

    part 2 day 1

--- day1.py
+++ day1.py
@@ -1,8 +1,17 @@
-def finding_entries(numbers, sum):
+def two_entries_multiplier(numbers, sum):
     for first_number in numbers:
         for second_number in numbers:
             if first_number + second_number == sum:
                 return first_number * second_number
+
+def three_entries_multiplier(numbers, sum):
+    for first_number in numbers:
+        for second_number in numbers:
+            if first_number + second_number < 2020:
+                for third_number in numbers:
+                    if first_number + second_number + third_number == sum:
+                        return first_number * second_number * third_number
+
 
 day1_input = open("day1_input", "r")
 lines = day1_input.read().split('\n')
@@ -11,5 +20,8 @@ numbers = []
 for line in lines:
     numbers.append(int(line))
 
-answer = finding_entries(numbers, 2020)
-print(answer)
+part1_answer = two_entries_multiplier(numbers, 2020)
+part2_answer = three_entries_multiplier(numbers, 2020)
+
+print(part1_answer)
+print(part2_answer)
