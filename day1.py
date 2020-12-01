9ad28ae9d95454f86444c9a8cde8aeac62908c43
Marcin Parda <marcin98parda@gmail.com>
2020-12-02 00:28:38 +0100

    refactor functions from day 1

--- day1.py
+++ day1.py
@@ -1,16 +1,29 @@
 def two_entries_multiplier(numbers, sum):
-    for first_number in numbers:
-        for second_number in numbers:
-            if first_number + second_number == sum:
-                return first_number * second_number
+    i = 0
+    while i < len(numbers):
+        j = i + 1
+        if numbers[i] < 2020:
+            while j < len(numbers):
+                if numbers[i] + numbers[j] == sum:
+                    return numbers[i] * numbers[j]
+                j += 1
+        i += 1
+
 
 def three_entries_multiplier(numbers, sum):
-    for first_number in numbers:
-        for second_number in numbers:
-            if first_number + second_number < 2020:
-                for third_number in numbers:
-                    if first_number + second_number + third_number == sum:
-                        return first_number * second_number * third_number
+    i = 0
+    while i < len(numbers):
+        j = i + 1
+        if numbers[i] < 2020:
+            while j < len(numbers):
+                if numbers[i] + numbers[j] < 2020:
+                    k = j + 1
+                    while k < len(numbers):
+                        if numbers[i] + numbers[j] + numbers[k] == sum:
+                            return numbers[i] * numbers[j] * numbers[k]
+                        k += 1
+                j += 1
+        i += 1
 
 
 day1_input = open("day1_input", "r")
