84550b1fe00c784de212beed281764ab74337628
Marcin Parda <marcin98parda@gmail.com>
2020-12-03 00:51:21 +0100

    day 2 part 2

--- day2.py
+++ day2.py
@@ -9,12 +9,28 @@ def password_is_valid(line):
     return False
 
 
+def password_is_valid_2(line):
+    splitted_line = line.split(" ")
+    password = splitted_line[2]
+    first_index = int(splitted_line[0].split("-")[0]) - 1
+    second_index = int(splitted_line[0].split("-")[1]) - 1
+    first_character = password[first_index]
+    second_character = password[second_index]
+    letter = splitted_line[1][0]
+    if (first_character == letter) != (second_character == letter):
+        return True
+    return False
+
+
 day2_input = open("day2_input", "r")
 
 valid_count = 0
+valid_count_2 = 0
 
 for line in day2_input.read().split("\n"):
     if password_is_valid(line):
         valid_count += 1
+    if password_is_valid_2(line):
+        valid_count_2 += 1
 
-print(valid_count)
+print(valid_count, valid_count_2)
